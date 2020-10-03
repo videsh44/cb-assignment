9c68c8208864dc568e503859f88d6596c18f9ce4
videsh <videsh.ghodarop@iaugmentor.com>
2020-10-03 11:11:11 +0530

    Updates

---
+++ precache-manifest.d565eb43eccde50c6384db6f1a1f614d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670fe03936c5da9f59b1ab510e6ad4d8",
+    "url": "/cb-assignment/index.html"
+  },
+  {
+    "revision": "ff5f65fb258920636302",
+    "url": "/cb-assignment/static/css/2.99ee2f66.chunk.css"
+  },
+  {
+    "revision": "6a3361bd05c6dac3b1f0",
+    "url": "/cb-assignment/static/css/main.d1191c2f.chunk.css"
+  },
+  {
+    "revision": "ff5f65fb258920636302",
+    "url": "/cb-assignment/static/js/2.1625ffc6.chunk.js"
+  },
+  {
+    "revision": "3526165a064f92e9d1298375f7b57ece",
+    "url": "/cb-assignment/static/js/2.1625ffc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a3361bd05c6dac3b1f0",
+    "url": "/cb-assignment/static/js/main.c04cd9a4.chunk.js"
+  },
+  {
+    "revision": "dd2cb63c47723d3d3366",
+    "url": "/cb-assignment/static/js/runtime-main.fcce3226.js"
+  }
+]);
